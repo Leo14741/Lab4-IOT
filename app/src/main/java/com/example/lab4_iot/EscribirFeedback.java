de72b995a20ccfc2defd06faac92730e3c9c2c29
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2023-10-24 18:56:16 -0500

    enviar feedback

--- app/src/main/java/com/example/lab4_iot/EscribirFeedback.java
+++ app/src/main/java/com/example/lab4_iot/EscribirFeedback.java
@@ -3,12 +3,24 @@ package com.example.lab4_iot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EscribirFeedback extends AppCompatActivity {
+
+
+    private EditText editTextoFeedback;
+    private Button btnFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escribir_feedback);
+
+
+
+
+
+
     }
 }
