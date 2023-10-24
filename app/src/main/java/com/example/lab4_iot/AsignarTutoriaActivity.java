ef436aa74e56dd482803f5c65a2faabefb5cdc17
leonardo <a20201696@pucp.edu.pe>
2023-10-24 03:50:22 -0500

    oa

--- app/src/main/java/com/example/lab4_iot/AsignarTutoriaActivity.java
+++ app/src/main/java/com/example/lab4_iot/AsignarTutoriaActivity.java
@@ -1,10 +1,13 @@
 package com.example.lab4_iot;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AsignarTutoriaActivity extends AppCompatActivity {
@@ -14,7 +17,7 @@ public class AsignarTutoriaActivity extends AppCompatActivity {
     private Button btnAsignar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asignar_tutoria);
 
@@ -27,50 +30,48 @@ public class AsignarTutoriaActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String codigoTutor = editTextCodigoTutor.getText().toString();
                 String idEmpleado = editTextIdEmpleado.getText().toString();
-
-                // Realizar la consulta a la base de datos para obtener el manager_id del empleado
-                // y validar si coincide con el código del tutor
-                boolean esManager = validarSiEsManager(codigoTutor, idEmpleado);
-
-                if (!esManager) {
-                    // Mostrar notificación de que el tutor no es el manager del empleado
-                    Toast.makeText(AsignarTutoriaActivity.this, "El tutor no es el manager del empleado", Toast.LENGTH_SHORT).show();
-                    return; // Salir de la función si la validación falla
-                }
-
-                // Realizar la consulta a la base de datos para verificar si el empleado ya tiene una cita asignada
-                boolean tieneCitaAsignada = validarSiTieneCitaAsignada(idEmpleado);
-
-                if (tieneCitaAsignada) {
-                    // Mostrar notificación de que el empleado ya tiene una cita asignada
-                    Toast.makeText(AsignarTutoriaActivity.this, "El trabajador ya tiene una cita asignada. Elija otro trabajador", Toast.LENGTH_SHORT).show();
-                    return; // Salir de la función si la validación falla
-                }
-
-                // Si pasa todas las validaciones, mostrar notificación de éxito
-                Toast.makeText(AsignarTutoriaActivity.this, "Asignación del trabajador correcta", Toast.LENGTH_SHORT).show();
+                new ObtenerManagerIdTask().execute(idEmpleado, codigoTutor);
             }
-
-            // Función para validar si el tutor es el manager del empleado
-            private boolean validarSiEsManager(String codigoTutor, String idEmpleado) {
-                // Aquí deberías realizar la consulta a la base de datos y comparar el manager_id del empleado con el código del tutor
-                // Si coincide, retornar true, de lo contrario, retornar false
-                // Por ejemplo:
-                // return managerIdDelEmpleado.equals(codigoTutor);
-                // Aquí debes reemplazar managerIdDelEmpleado con tu consulta a la base de datos
-                return false;
-            }
-
-            // Función para validar si el empleado ya tiene una cita asignada
-            private boolean validarSiTieneCitaAsignada(String idEmpleado) {
-                // Aquí deberías realizar la consulta a la base de datos para verificar si el empleado ya tiene una cita asignada
-                // Si tiene una cita asignada, retornar true, de lo contrario, retornar false
-                // Por ejemplo:
-                // return citaAsignada;
-                // Aquí debes reemplazar citaAsignada con tu consulta a la base de datos
-                return false;
-            }
-
         });
     }
+
+    private class ObtenerManagerIdTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String idEmpleado = params[0];
+            String codigoTutor = params[1];
+            // Aquí realiza la solicitud HTTP al servidor Node.js para obtener el manager_id del empleado
+            // y luego maneja la respuesta en onPostExecute
+            return null; // Debes reemplazar esto con tu lógica de obtención del manager_id
+        }
+
+        @Override
+        protected void onPostExecute(String managerId) {
+            if (managerId != null && managerId.equals(editTextCodigoTutor.getText().toString())) {
+                new ValidarCitaTask().execute(editTextIdEmpleado.getText().toString());
+            } else {
+                Toast.makeText(AsignarTutoriaActivity.this, "El tutor no es el manager del empleado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class ValidarCitaTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String idEmpleado = params[0];
+            // Aquí realiza la solicitud HTTP al servidor Node.js para verificar si el empleado tiene una cita asignada
+            // y luego maneja la respuesta en onPostExecute
+            return null; // Debes reemplazar esto con tu lógica de validación de cita
+        }
+
+        @Override
+        protected void onPostExecute(Boolean tieneCitaAsignada) {
+            if (tieneCitaAsignada) {
+                Toast.makeText(AsignarTutoriaActivity.this, "El trabajador ya tiene una cita asignada. Elija otro trabajador", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(AsignarTutoriaActivity.this, "Asignación del trabajador correcta", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
+
